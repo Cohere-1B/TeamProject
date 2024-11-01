5d2d6a2558ed7a0b69006c52fd14815c0eaf570a
divya-subramonian25 <divya.subramonian25@gmail.com>
2024-11-01 16:02:33 -0700

    ds - updated html files

--- frontend/homepage.py
+++ frontend/homepage.py
@@ -4,12 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1>Welcome to BrandMatch!</h1>"
+    return render_template('index.html')
 
 @app.route('/about')
 def about():
-    return "<p>Products List</p>"
+    return render_template('products.html')
 
-# Run the app
 if __name__ == "__main__":
     app.run(debug=True)
